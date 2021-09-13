1935da7560c8b6cc7b43ab638086489944deac5f
Marie Barati <barati.it91@gmail.com>
2021-09-12 20:24:35 -0700

    Add Contact Dto

---
+++ Dtoes/ContactDto.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace contactManagementBackend.Dtoes
+{
+     public record ContactDto
+    {
+        
+        public Guid Id {set; get;}
+        
+        public string FirstName {set; get;}
+        public string LastName{set; get;}
+        public string Email {set; get;}
+        public string HomePhoneNumber {set; get;}
+        public string BusinessPhoneNumber {set; get;}
+        public string MobilePhoneNumber {set; get;}
+    }
+}
